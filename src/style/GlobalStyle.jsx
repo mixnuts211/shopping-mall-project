665d5545eee7dab12ab7b906258c25f33689c60c
mixnuts211 <migyeong0211@gmail.com>
2023-01-16 10:58:52 +0900

    Style : 전역으로 사용될 css 스타일 추가 설정

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, css } from "styled-components";
 import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
@@ -34,4 +34,36 @@ export const GlobalStyle = createGlobalStyle`
     input {
       font: inherit;
     }
+`;
+
+export const PageWrap = css`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ConWrap = css`
+  width: 1280px;
+  flex-grow: 1;
+`;
+
+export const LayoutWrap = css`
+  margin: 0 auto;
+`;
+
+export const IR = css`
+  position: absolute;
+  clip-path: inset(50%);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+`;
+
+export const Ellipsis = css`
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
