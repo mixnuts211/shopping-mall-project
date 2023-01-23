5a2ad7acda25b349c2a9790754a7a14f6a88ea13
mixnuts211 <migyeong0211@gmail.com>
2023-01-23 21:29:17 +0900

    Feat : 404페이지 라우터 error 처리

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 import Root from "./Root";
 
 export const router = createBrowserRouter([
@@ -7,5 +8,6 @@ export const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     children: [{}],
+    errorElement: <NotFound />,
   },
 ]);
