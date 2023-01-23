d61df8fea0653db92e08ccc74b15da9637398957
mixnuts211 <migyeong0211@gmail.com>
2023-01-23 21:28:35 +0900

    Feat : index.jsx RouterProvider 추가

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Root from "./Root";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Root />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
