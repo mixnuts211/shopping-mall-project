164bba38401142ef29b58f2ea4a2e89b746d553a
mixnuts211 <migyeong0211@gmail.com>
2023-02-07 16:52:29 +0900

    Chore : 타입스크립트 설치 및 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+// const container = document.getElementById("root");
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
